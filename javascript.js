c911587d407df6fc13439068881372e56d741366
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-19 14:11:49 -0300

    Init setup of grid container

---
+++ javascript.js
@@ -0,0 +1,4 @@
+const container = document.querySelector("#container");
+const gridContainer = document.createElement('div');
+document.body.appendChild(gridContainer);
+gridContainer.setAttribute('class', 'gridContainer');
