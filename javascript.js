3b2dc66abe5bf8f769f24861323464d57cc942d7
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-20 16:07:35 -0300

    Create grid elements

--- javascript.js
+++ javascript.js
@@ -1,4 +1,22 @@
-const container = document.querySelector("#container");
+const container = document.querySelector("#container")
 const gridContainer = document.createElement('div');
 document.body.appendChild(gridContainer);
 gridContainer.setAttribute('class', 'gridContainer');
+
+function makeGrid(gridDimension=16) {
+
+  gridContainer.style.gridTemplateColumns = `repeat( ${gridDimension}, 1fr)`;
+
+
+  for (i = 0; i < (gridDimension**2); i++) {
+
+    let gridElement = document.createElement('div');
+    gridElement.setAttribute('class', 'gridElement');
+    gridContainer.appendChild(gridElement);
+
+  }
+
+}
+
+
+makeGrid();
