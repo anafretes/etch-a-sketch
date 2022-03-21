ea2bae75b0d325698756b8992d97aef0a2b223a7
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-21 20:29:42 -0300

    Make cells change color on hover
    Tried to do the darken thing but it took me to dark places. Will try 
    again later.

--- javascript.js
+++ javascript.js
@@ -1,49 +1,80 @@
 const container = document.querySelector("#container")
+
 const gridContainer = document.createElement('div');
 document.body.appendChild(gridContainer);
+
 gridContainer.setAttribute('class', 'gridContainer');
 
 const button = document.createElement('button');
 button.textContent = "Change the size of the board";
 container.appendChild(button);
 
+
 button.addEventListener('click', function(){
+
   button.value = prompt("Enter a positive integer: ", 16);
   clearGrid();
   makeGrid(button.value);
+  paintCell();
 });
 
 
 function makeGrid(gridDimension=16) {
 
+  if (gridDimension > 100) {
+    alert("Grid is too large. Please enter a number smaller than 100");
+    makeGrid();
+  } else if (gridDimension < 1 ) {
+    gridDimension = 16;
+    alert("Grid can't be built. Please try again with a positive integer.");
+    makeGrid();
+  } else {
+    gridContainer.style.gridTemplateColumns = `repeat( ${gridDimension}, 1fr)`;
 
-  gridContainer.style.gridTemplateColumns = `repeat( ${gridDimension}, 1fr)`;
-
-
-  for (i = 0; i < (gridDimension**2); i++) {
-
-    const gridElement = document.createElement('div');
-    gridElement.setAttribute('class', 'gridElement');
-    gridContainer.appendChild(gridElement);
-
+    for (i = 0; i < (gridDimension**2); i++) {
+      const gridElement = document.createElement('div');
+      gridElement.setAttribute('id', `${i}`);
+      gridElement.setAttribute('class', 'gridElement');
+      gridContainer.appendChild(gridElement);
+    }
   }
-
 }
 
+
 function clearGrid() {
+
   const elements = document.querySelectorAll('.gridElement');
   for (element of elements) {
     gridContainer.removeChild(element);
   }
 }
-function paintCell() {
-  let items = document.querySelectorAll('.gridElement');
-  let itemLen = items.length;
-  console.log(itemLen);
-  let color= Math.floor(Math.random() * 16777215);
-  color = `#${color.toString(16)}`;
-  console.log(color);
 
+
+function paintCell() {
+
+  let items = document.querySelectorAll('.gridElement');
+
+  let colorRgb = "rgb(";
+  let rgbArr = [];
+  for (let i = 0; i < 3; i++) {
+    let generate = Math.floor(Math.random() * 256);
+    rgbArr.push(generate);
+    if (i < 2) {
+      colorRgb += `${generate}, `;
+    } else {
+      colorRgb += `${generate})`;
+    }
+  }
+
+
+  items.forEach(function (item) {
+
+    item.addEventListener('mouseenter', () => {
+      item.setAttribute('style', `background-color: ${colorRgb}`);
+      let itemColor = item.style.backgroundColor;
+      console.log(itemColor);
+    });
+  });
 }
 
 
